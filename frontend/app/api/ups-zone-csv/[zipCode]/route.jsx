b4a75c0bee4525791cfd1bbc1e435491b85a557d
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-02-08 13:44:10 +0530

    API Revert

--- frontend/app/api/ups-zone-csv/[zipCode]/route.jsx
+++ frontend/app/api/ups-zone-csv/[zipCode]/route.jsx
@@ -3,49 +3,19 @@ import { NextResponse } from "next/server";
 export async function GET(request, { params }) {
   const { zipCode } = await params;
   const origin_prefix = zipCode.slice(0, 3);
-  
   const response = await fetch(
     `https://www.ups.com/media/us/currentrates/zone-csv/${origin_prefix}.xls`
   );
 
-  console.log("Respons from UPS Zone CSV");
-
-  if (!response.ok) {
-    return NextResponse.json({ error: "File not found" }, { status: 404 });
+  if (response.ok) {
+    const blob = await response.blob();
+    return new NextResponse(blob, {
+      headers: {
+        "Content-Type": "application/vnd.ms-excel",
+        "Content-Disposition": `attachment; filename=${origin_prefix}.xls`,
+      },
+    });
   }
-  
-  const reader = response.body.getReader();
-  const stream = new ReadableStream({
-    start(controller) {
-      function push() {
-        reader.read().then(({ done, value }) => {
-          if (done) {
-            controller.close();
-            return;
-          }
-          controller.enqueue(value);
-          push();
-        });
-      }
-      push();
-    },
-  });
 
-  return new NextResponse(stream, {
-    headers: {
-      "Content-Type": "application/vnd.ms-excel",
-      "Content-Disposition": `attachment; filename=${origin_prefix}.xls`,
-    },
-  });
-  //   if (response.ok) {
-  //     const blob = await response.blob();
-  //     return new NextResponse(blob, {
-  //       headers: {
-  //         "Content-Type": "application/vnd.ms-excel",
-  //         "Content-Disposition": `attachment; filename=${origin_prefix}.xls`,
-  //       },
-  //     });
-  //   }
-
-  //   return NextResponse.json({ error: "File not found" }, { status: 404 });
+  return NextResponse.json({ error: "File not found" }, { status: 404 });
 }
